f641acb6ccda0fda0925ebd4b13c09f5fd99d995
bluebasooo <blyayachapoveshuci@yandex.ru>
2023-04-09 21:12:22 +0300

    start

--- code/Text.h
+++ code/Text.h
@@ -1,10 +1,19 @@
 #pragma once
 #include "Word.h"
 #include <vector>
+#include <string>
+#include <fstream>
 
 class Text {
 public:
     Text(std::string text);
+    Text(std::ifstream text);
+
+    void next();
 private:
+    static const int bunch = 100;
+    static int start;
+    std::string text;
+    std::ifstream text_file;
     std::vector<Word> word;
 };
