1cabeead61b70a454ee7142e2d5013775ae813b4
pelmenman <akula_from_ikea@mail.ru>
2023-04-09 21:31:59 +0300

    Levenshtein is added

--- code/Levenshtein.cpp
+++ code/Levenshtein.cpp
@@ -1,11 +1,25 @@
 #include "Levenshtein.h"
 
 Levenshtein Levenshtein::getInstance() {
+
     if(realization == 0) {
         realization = new Levenshtein();
         return *realization;
     }
     return *realization;
+}
+
+int Levenshtein:: getMinDistance(Word word, int sizeWord, Word otherWord, int sizeOtherWord) {
+    if (min(sizeWord, sizeOtherWord) == 0) return max(sizeWord, sizeOtherWord);
+
+    int distance;
+
+    (word[sizeWord-1] == otherWord[sizeOtherWord - 1])? distance = 0 : distance = 1;
+
+    return min(getMinDistance(word, sizeWord - 1, otherWord, sizeOtherWord - 1) + distance, 
+           min(getMinDistance(word, sizeWord, otherWord, sizeOtherWord - 1) + 1, 
+           getMinDistance(word, sizeWord - 1, otherWord, sizeOtherWord) + 1));
+
 }
 
 Levenshtein::Levenshtein() {}
--- code/Levenshtein.h
+++ code/Levenshtein.h
@@ -3,6 +3,7 @@
 class Levenshtein {
 public:
     static Levenshtein getInstance();
+    static getMinDistance(Word word, int sizeWord, Word otherWord, int sizeOtherWord);
 private:
     Levenshtein();
     static Levenshtein* realization;
